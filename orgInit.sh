a37605afce949d41253287543e1fcd2d347d10e0
esandoval30 <esandoval@salesforce.com>
2020-05-22 12:30:12 +0100

    Removing 's

--- orgInit.sh
+++ orgInit.sh
@@ -3,10 +3,8 @@ sfdx force:org:create -f config/project-scratch-def.json -d 7 -s -w 3
 sfdx force:source:push
 #create user
 sfdx force:user:password:generate
-
 #Create 'ALL AEs' Account to be the parent of all AE Contact Records
-sfdx force:data:record:create -s Account -v "Name='ALL AEs' External_ID__c=1"
-
+sfdx force:data:record:create -s Account -v "Name=ALLAEs External_ID__c=1"
 #Bulk Load ALL AEs into Contact Object
 sfdx force:data:bulk:upsert -s Contact -f data/AEs-1.csv -i External_ID__c
 #Bulk Load ALL Client (Accounts) linked to their corresponding AE
