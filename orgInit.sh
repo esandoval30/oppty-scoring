a8ff5d9a19b7f3bd5389f8fd68d4fc4ba6471a3d
esandoval30 <esandoval@salesforce.com>
2020-06-01 17:38:10 +0100

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -11,5 +11,5 @@ sfdx force:data:bulk:upsert -s Contact -w 60 -f data/AEs-1.csv -i External_ID__c
 sfdx force:data:bulk:upsert -s Account -w 60 -f data/Account.csv -i External_ID__c
 #Bulk Load ALL Opportunities
 sfdx force:data:bulk:upsert -s Opportunity -w 60 -f data/AllOpportunities.csv -i External_ID__c
-#open the scratch org -- https://login.salesforce.com/packaging/installPackage.apexp?p0=04t58000000SGw3
+#open the scratch org
 sfdx force:org:open -p /lightning/setup/SetupOneHome/home
