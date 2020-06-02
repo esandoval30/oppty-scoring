869143fae24a6e8be4e8be8b6a00042b7ec234ca
esandoval30 <esandoval@salesforce.com>
2020-06-02 13:14:39 +0100

    Update orgInit.sh
    Adjusting Opportunity Close Date  before bulk loading

--- orgInit.sh
+++ orgInit.sh
@@ -9,7 +9,8 @@ sfdx force:data:record:create -s Account -v "Name='ALL AEs' External_ID__c=1"
 sfdx force:data:bulk:upsert -s Contact -w 60 -f data/AEs-1.csv -i External_ID__c
 #Bulk Load ALL Client (Accounts) linked to their corresponding AE
 sfdx force:data:bulk:upsert -s Account -w 60 -f data/Account.csv -i External_ID__c
-#Bulk Load ALL Opportunities
-sfdx force:data:bulk:upsert -s Opportunity -w 60 -f data/AllOpportunities.csv -i External_ID__c
+#Bulk Load ALL Opportunities (adjusting closedate before load)
+sfdx shane:data:dates:update -r 5-21-2020
+sfdx force:data:bulk:upsert -s Opportunity -w 60 -f data-modified/AllOpportunities.csv -i External_ID__c
 #open the scratch org
 sfdx force:org:open -p /lightning/setup/SetupOneHome/home
