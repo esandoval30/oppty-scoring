4e7bed9514239c93b84f43324f39a0d3ee7b2a90
esandoval30 <esandoval@salesforce.com>
2020-07-29 08:52:29 +0100

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 7 -s -w 3
+sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 3
 #push source code into scratch org
 sfdx force:source:push
 #create user
